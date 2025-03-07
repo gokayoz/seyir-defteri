bff01599e8c6416599cc85b6b951d0e33d2fa238
gokayoz <gokay.oz@outlook.com.tr>
2025-03-07 19:05:50 +0300

    Adds Gonderim class

---
+++ SeyirDefteri.Core/Gonderim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeyirDefteri.Core
+{
+    public class Gonderim
+    {
+        public int GonderimId { get; set; }
+        public Urun Urun { get; set; }
+        public IlgilenenKisi IlgilenenKisi { get; set; }
+        public SeyirKaydi SeyirKaydi { get; set; }
+        public decimal GonderimTonaji { get; set; }
+    }
+}
