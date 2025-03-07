03fc61c8a0b77f2258246ba1dc379c4b034d464c
gokayoz <gokay.oz@outlook.com.tr>
2025-03-07 19:08:01 +0300

    Adds Urun class

---
+++ SeyirDefteri.Core/Urun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeyirDefteri.Core
+{
+    public class Urun
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; }
+    }
+}
