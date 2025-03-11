081be8c1ac46284f29a6837a8444f67a8ad27b9b
gokayoz <gokay.oz@outlook.com.tr>
2025-03-11 16:45:48 +0300

    Adds transition to new form

---
+++ SeyirDefteri.UI/FRMZRaporuEkrani.cs
@@ -0,0 +1,26 @@
+﻿using SeyirDefteri.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeyirDefteri.UI
+{
+    public partial class FRMZRaporuEkrani : Form
+    {
+        private List<Gonderim> Gonderimler;
+        public FRMZRaporuEkrani()
+        {
+            InitializeComponent();
+        }
+        public FRMZRaporuEkrani(List<Gonderim> gonderimler) : this()
+        {
+            Gonderimler = gonderimler;
+        }
+    }
+}
